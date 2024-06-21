093e75dbf950dcc1712429be9923aabfaff29e9f
taiwaness <arbula@gmail.com>
2024-06-21 12:08:43 +0200

    scripts under development

---
+++ dev/setup.R
@@ -0,0 +1,45 @@
+library(usethis)
+library(devtools)
+
+## This script is for setting up development dependencies and should be run once during initial setup or when updating dependencies.
+#create_package("/omics/groups/OE0219/internal/MJMC/Tools/quothic")
+
+## Optionally set up GitHub PAT for installations from GitHub
+# usethis::edit_r_environ()
+
+## Install necessary packages from GitHub if not available on CRAN or specific versions are needed
+# devtools::install_github("jlmelville/uwot@v0.1.16")
+# devtools::install_github("satijalab/sctransform@v0.3.5")
+# devtools::install_github("satijalab/seurat@v4.3.0")
+# devtools::install_github('satijalab/seurat-data')
+#if (!require(moments)) install.packages("moments")
+
+
+
+## Add dependencies to DESCRIPTION
+use_package("Seurat", type = "Imports")
+use_package("dplyr", type = "Imports")
+use_package("tidyr", type = "Imports")
+use_package("ggplot2", type = "Imports")
+use_package("mclust", type = "Imports")
+use_package("moments", type = "Imports")
+use_package("SeuratData", type="Imports")
+
+library(devtools)
+devtools::document()
+
+#devtools::load_all()
+
+# remove.packages("quothic")
+# Restart R session
+setwd("/omics/groups/OE0219/internal/MJMC/Tools/quothic")
+devtools::install(upgrade = "never")
+
+
+
+library(usethis)
+use_testthat()
+use_test("shannon_entropy")
+
+use_vignette("introduction_to_quothic")
+
